f8e9be434ccbd2065741cdc80413fa35f952bbe8
Rennan Rocha <rennancge@hotmail.com>
2017-11-10 15:16:28 -0300

    Melhorada no design de login

--- geekway-web/js/app.js
+++ geekway-web/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ['ui.router']);
+var app = angular.module("app", ['ui.router', 'ngCookies', 'ngMessages', 'ngMaterial', 'material.svgAssetsCache']);
 
 app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
